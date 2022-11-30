f922fc29cd623d916a5739ad544a6b390ac9109c
Md. Rabiul Islam <91551048+rabiul121@users.noreply.github.com>
2022-11-30 14:00:01 +0600

    need buh fixing

--- TheOdinProject/rock paper scissors/game.js
+++ TheOdinProject/rock paper scissors/game.js
@@ -10,37 +10,52 @@ function getComputerChoice() {
     }
 }
 
-/* Rock wins against scissors;      playerSelection === "rock" && computerSelection === "scissors"
-paper wins against rock;            playerSelection === "paper" && computerSelection === "rock"
-and scissors wins against paper.    playerSelection === "scissors" && computerSelection === "paper"
-If both players throw the same hand signal, it is considered a tie, and play resumes until there is a clear winner. */
 
 // create a function that plays a single round of the game
 function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt("Type 'rock', 'paper' or 'scissors'").toLocaleLowerCase();
+    console.log(playerSelection);
+    computerSelection = getComputerChoice();
+    console.log(computerSelection);
     if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You Win! Rock beats Scissors!";
+        return "win";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You Win! Paper beats Rock!";
+        return "win";
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You Win! Scissors beats Paper!";
+        return "win";
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        "You Lose! Rock beats Scissors!";
+        return "lose";
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You Lose! Paper beats Rock!";
+        return "lose";
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You Lose! Scissors beats Paper!"
+        return "lose"
     } else if ((playerSelection === "rock" && computerSelection === "rock") || (playerSelection === "paper" && computerSelection === "paper") || (playerSelection === "scissors" && computerSelection === "scissors")) {
-        return "The game is a tie! Try again!"
+        return "tie"
     }
 }
 
+// main game function
+function game() {
+    let playerWins = 0;
+    let computerWins = 0;
+    for (let i = 0; i < 5; i++) {
+        if (playRound() == "win") {
+            playerWins += 1;
+        } else if (playRound() == "lose") {
+            computerWins += 1;
+        } else if (playRound() == "tie") {
+            playerWins += 0;
+            computerWins += 0;
+        }
+    }
+    if (playerWins > computerWins) {
+        console.log(playerWins + "\n" + computerWins + "\n" + "You won the game!");
+    } else if (playerWins < computerWins) {
+        console.log(playerWins + "\n" + computerWins + "\n" + "You lose the game!");
+    } else if (playerWins == computerWins) {
+        console.log(playerWins + "\n" + computerWins + "\n" + "The game is a tie!");
+    }
+}
 
-
-
-// console.log(getComputerChoice());
-
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
-// console.log("You've selected: " + playerSelection);
-// console.log("Computer selected: " + computerSelection);
-// console.log(playRound(playerSelection, computerSelection));
+// test the game
+game();
